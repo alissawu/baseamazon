cb7ecdf073be6315f3da25b08742471b59ef9cb4
lauren.walker@duke.edu <lgw17@duke.edu>
2024-10-22 05:44:56 -0400

    debug

--- app/sellers.py
+++ app/sellers.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 from flask_login import current_user
 from app.models.product import Product
-from app.models.sellers import Seller
+from app.sellers import Seller
 
 bp = Blueprint('sellers', __name__)
 
