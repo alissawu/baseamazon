cc0b4f7f41fc30aa1aae97f16befadb79a921f49
lauren.walker@duke.edu <lgw17@duke.edu>
2024-10-23 19:05:45 -0400

    debug

--- app/sellers.py
+++ app/sellers.py
@@ -66,5 +66,6 @@ def sellers_inventory():
 # implement search
 @bp.route('/sellers/<int:acct_ID>', methods=['GET'])
 def get_seller_products(acct_ID):
+    acct_ID = request.args.get('acct_id')
     products = Seller.get_products_by_seller_id(acct_ID)
     return render_template('sellers.html', products=products)
