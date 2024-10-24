e63b4bf7602ffab7628f1f642c7f7e0d993dcf33
lauren.walker@duke.edu <lgw17@duke.edu>
2024-10-24 13:44:43 -0400

    fix error

--- app/cart.py
+++ app/cart.py
@@ -1,8 +1,8 @@
 from flask import render_template, redirect, url_for, abort, request, current_app as app
 from flask_login import current_user
 from decimal import Decimal
-# from .cart import Cart
-from .models.order import Order
+# from app.cart import Cart
+# from app.order import Order
 from flask import Blueprint
 
 bp = Blueprint('carts', __name__)
