7295493fe5d7ac5e15e009b896d8f2cefb249556
lauren.walker@duke.edu <lgw17@duke.edu>
2024-10-24 13:39:04 -0400

    fix error

--- app/__init__.py
+++ app/__init__.py
@@ -46,8 +46,8 @@ def create_app():
     from .sellers import bp as sellers_bp
     app.register_blueprint(sellers_bp)
 
-    from .carts import bp as carts_bp
-    app.register_blueprint(carts_bp)
+    from .carts import bp as cart_bp
+    app.register_blueprint(cart_bp)
 
 
 
