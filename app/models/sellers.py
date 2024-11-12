6112da48bd452f81ec34698c5ebdd0dcbff014fc
lauren.walker@duke.edu <lgw17@duke.edu>
2024-11-12 02:36:31 -0500

    add quantity to sellers

--- app/models/sellers.py
+++ app/models/sellers.py
@@ -2,19 +2,20 @@ from flask import current_app as app
 
 
 class Seller:
-    def __init__(self, acct_ID=None, product_ID=None, name=None, price=None, available=None):
+    def __init__(self, acct_ID=None, product_ID=None, name=None, price=None, available=None, quantity=None):
         self.acct_ID = acct_ID
         self.product_ID = product_ID
         self.name = name
         self.price = price
         self.available = available
+        self.quantity = quanitity
 
 
     # Get products for a specific seller
     @staticmethod
     def get_products_by_seller_id(acct_ID):
         rows = app.db.execute('''
-        SELECT Seller.acct_ID, Products.id, Products.name, Products.price, Products.available
+        SELECT Seller.acct_ID, Products.id, Products.name, Products.price, Products.available, Seller.quanitity
         FROM Seller
         JOIN Products ON Products.id = Seller.product_ID
         WHERE Seller.acct_ID = :acct_ID
@@ -25,7 +26,7 @@ class Seller:
     @staticmethod
     def get_products_not_in_inventory(acct_ID):
         rows = app.db.execute('''
-        SELECT Products.id, Products.name, Products.price, Products.available
+        SELECT Products.id, Products.name, Products.price, Products.available, Seller.quantity
         FROM Products
         WHERE Products.id NOT IN (
             SELECT product_ID
@@ -38,7 +39,8 @@ class Seller:
             product_ID=int(row[0]),  # Ensure Product ID is an integer
             name=str(row[1]),        # Ensure Product Name is a string
             price=float(row[2]),     # Ensure Product Price is a float
-            available=bool(row[3])   # Ensure Product Availability is a boolean
+            available=bool(row[3]),  # Ensure Product Availability is a boolean
+                quantity=str([row[4]])
             ) for row in rows]
 
     # Add a product to the seller's inventory
@@ -50,6 +52,13 @@ class Seller:
         ''', acct_ID=acct_ID, product_ID=product_ID)
 
     # Remove a product from a seller's inventory
+    @staticmethod
+    def remove_product_from_inventory(acct_ID, product_ID):
+        app.db.execute('''
+        DELETE FROM Seller
+        WHERE acct_ID = :acct_ID AND product_ID = :product_ID
+        ''', acct_ID=acct_ID, product_ID=product_ID)
+    # 
     @staticmethod
     def remove_product_from_inventory(acct_ID, product_ID):
         app.db.execute('''
