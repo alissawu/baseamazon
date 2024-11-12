16d108c7362c32c254de86cb62e6da49013fe220
lauren.walker@duke.edu <lgw17@duke.edu>
2024-11-12 01:41:09 -0500

    debug

--- app/models/sellers.py
+++ app/models/sellers.py
@@ -33,7 +33,7 @@ class Seller:
             WHERE acct_ID = :acct_ID
             )
             ''', acct_ID=acct_ID)
-        print("rows")
+        print("Query Result Rows:", rows)  # Debugging: Print rows to verify column order
         return [Seller(row[0], row[1], row[2], row[3]) for row in rows]
 
     # Add a product to the seller's inventory
