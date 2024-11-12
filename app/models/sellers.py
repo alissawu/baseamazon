690041e85d4eb8972d9c6000d35250b8e4103d70
lauren.walker@duke.edu <lgw17@duke.edu>
2024-11-12 00:59:33 -0500

    fix sellers_add.html

--- app/models/sellers.py
+++ app/models/sellers.py
@@ -25,7 +25,7 @@ class Seller:
     @staticmethod
     def get_products_not_in_inventory(acct_ID):
         rows = app.db.execute('''
-        SELECT Products.id, Products.name, Products.price
+        SELECT Products.id, Products.name, Products.price, Products.available
         FROM Products
         WHERE Products.id NOT IN (
             SELECT product_ID
