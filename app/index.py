2c487ad328cc31e6c46cc19bd2d208a494bb3eda
lauren.walker@duke.edu <lgw17@duke.edu>
2024-12-06 14:08:54 -0500

    debug

--- app/index.py
+++ app/index.py
@@ -11,8 +11,8 @@ bp = Blueprint('index', __name__)
 
 @bp.route('/')
 def index():
-    sort_order = request.args.get('sort_order', 'ASC')  # Default is 'ASC'
-    available = request.args.get('available', True)
+    sort_order = request.args.get('sort_order', 'ASC')  # default is 'ASC'
+    available = request.args.get('available', True) # defaul is true
     products = Product.get_all(available=available, sort_by_price=True, sort_order=sort_order)
 
     acct_ID = request.args.get('acct_ID')
